9919362684d0a29603ccf5b763d1760414cb47b2
test <68767710+namakemono-san@users.noreply.github.com>
2026-04-10 19:48:28 +0900

    fix: osu!からmurasame!に変更

--- osu.Desktop/Program.cs
+++ osu.Desktop/Program.cs
@@ -21,9 +21,9 @@ namespace osu.Desktop
     public static class Program
     {
 #if DEBUG
-        private const string base_game_name = @"osu-development";
+        private const string base_game_name = @"murasame-development";
 #else
-        private const string base_game_name = @"osu";
+        private const string base_game_name = @"murasame";
 #endif
 
         private static LegacyTcpIpcProvider? legacyIpc;
--- osu.Game/OsuGameBase.cs
+++ osu.Game/OsuGameBase.cs
@@ -77,9 +77,9 @@ namespace osu.Game
     public partial class OsuGameBase : Framework.Game, ICanAcceptFiles, IBeatSyncProvider
     {
 #if DEBUG
-        public const string GAME_NAME = "osu! (development)";
+        public const string GAME_NAME = "murasame! (development)";
 #else
-        public const string GAME_NAME = "osu!";
+        public const string GAME_NAME = "murasame!";
 #endif
 
         public const string OSU_PROTOCOL = "osu://";
